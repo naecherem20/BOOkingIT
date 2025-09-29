3cbbeaba308bb6ffbd3523920c0ffd27d9050c28
perpetual <naecherem20@gmail.com>
2025-09-29 07:04:21 +0100

    added deletion of booking by user

--- crud/user_crud.py
+++ crud/user_crud.py
@@ -1,4 +1,5 @@
 from models.user_model import Service
+from fastapi import HTTPException, status
 from sqlmodel import Session, select
 from schema.service_schema import Services
 from uuid import UUID
@@ -18,5 +19,13 @@ class UserCRUDService:
             book=self.session.exec(select(Service).where(Service.booking_id==user_id)).all()
             return book
         return []
+    def delete_book(self, book_id:UUID, user_id:UUID):
+        del_book=self.session.exec(select(Service).where(Service.id==book_id, Service.booking_id==user_id)).first()
+        if not del_book:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+        self.session.delete(del_book)
+        self.session.commit()
+        return {"message":f"{del_book.title} deletion was successsful"}
+
 
 
--- router/user_crud_router.py
+++ router/user_crud_router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,status,HTTPException
 from crud.user_crud import UserCRUDService
 from sqlmodel import Session
 from database.connection import get_session
 from schema.service_schema import Services
 from auth_config.user_login_auth import get_current_user
+from uuid import UUID
 
 
 
@@ -18,3 +19,8 @@ def create_book(book_in:Services, session: Session=Depends(get_session), current
 def get_all_booked_by_user(session:Session=Depends(get_session),current_user=Depends(get_current_user)):
    service=UserCRUDService(session)
    return service.get_all_booked_by_user(current_user.id)
+
+@router.delete("/", status_code=status.HTTP_404_NOT_FOUND)
+def delete_book(book_id:UUID, session:Session=Depends(get_session), current_user=Depends(get_current_user)):
+   service=UserCRUDService(session)
+   return service.delete_book(book_id, current_user.id)
